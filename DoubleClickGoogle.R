5ab79fd854068e1dee11ce38e450de56dcd4fe3a
Marcelo Tibau <marcelo.tibau@gate.cx>
2018-05-16 13:08:59 -0300

    Starting file

---
+++ DoubleClickGoogle.R
@@ -0,0 +1,19 @@
+# setwd("~/Documents/FLAGCX/Projects/DoubleClickBM")
+
+# API Request
+api.request <- function(url, querystring="",method="GET"){
+  
+  if(DC.authmethod=='oauth') {
+    if(method=="GET") {
+      req <- GET(url,query=querystring,config(token = DC.token))
+    } else if (method=="POST") {
+      # requires a dummy body to avoid a content-length error
+      req <- POST(url=paste0(url,'?',querystring),config=config(token = DC.token),body=list('dummy'),encode='json')
+    }
+  }
+  
+  stop_for_status(req)
+  response <- content(req,as='text')
+  
+  return(response)
+}
